16521d831082ba048c73927aba4fefc389b55837
mitakkshdoseja <67427555+mitakkshdoseja@users.noreply.github.com>
2020-10-25 21:13:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var gamestate = "start";
+var boxleft,boxLeftbody,boxbottom,boxBottomBody,boxright,boxRightBody,boxPosition,boxY;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var engine,world
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	engine = Engine.create();
+	world = engine.world;
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	boxPosition = width/2 - 100
+	boxY = 610
+
+	boxleft = createSprite(boxPosition,boxY,20,100);
+	boxleft.shapeColor=color(255,0,0);
+
+	boxLeftbody = Bodies.rectangle(boxPosition+20, boxY, 20,100 ,{isStatic:true});
+	World.add(world, boxLeftbody);
+
+	boxbottom=createSprite(boxPosition + 100, boxY+40, 200,20);
+	boxbottom.shapeColor=color(255,0,0);
+
+	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY + 40, 200,20 , {isStatic:true} );
+	 World.add(world, boxBottomBody);
+	 
+	 boxright=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxright.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed()
+ {
+	 if (gamestate != "stop")
+	 {
+
+	if (keyCode === LEFT_ARROW)
+	 {
+
+		helicopterSprite.x=helicopterSprite.x-20;    
+		translation={x:-20,y:0}
+		Matter.Body.translate(packageBody, translation)
+	
+	
+	  } else if (keyCode === RIGHT_ARROW) 
+	  {
+		helicopterSprite.x=helicopterSprite.x+20;
+		translation={x:20,y:0}
+		Matter.Body.translate(packageBody,translation);
+	  }
+	  else if (keyCode === DOWN_ARROW)
+	   {
+		Matter.Body.setStatic(packageBody,false);
+		gamestate = "stop"
+		}
+	}
+}
